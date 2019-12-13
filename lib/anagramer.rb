0e6f18a60038fd50bd6ea7f337afc4b2062d849a
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-12-13 15:06:14 -0800

    write test for mutiple inputted words to make an anagram successfully

--- lib/anagramer.rb
+++ lib/anagramer.rb
@@ -1,37 +1,66 @@
 
 
 
+
+
 class Anagramer
 
-  def check_if_anagram (input1, input2)
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
+  end
 
-  if input1.length != input2.length
-      puts "This is NOT an anagram!"
-      return "This is NOT an anagram!"
-    end
-    if input2.downcase.chars.sort == input1.downcase.chars.sort
+  def check_if_anagram ()
+    # if @input1.length != @input2.length
+    #   puts "This is NOT an anagram!"
+    #   return "This is NOT an anagram!"
+    # end
+    if @input2.downcase.chars.sort == @input1.downcase.chars.sort
       puts "This is an ANAGRAM!"
       return "This is an ANAGRAM!"
     end
-    input1_array = input1.downcase.chars.sort
-    input2_array = input1.downcase.chars.sort
+
+    sentence_array1 = @input1.gsub(/\W/, ' ').delete(' ').downcase.chars.sort
+    sentence_array2 = @input2.gsub(/\W/, ' ').delete(' ').downcase.chars.sort
+    if sentence_array1 == sentence_array2
+      puts "This is an ANAGRAM!"
+      return "This is an ANAGRAM!"
+    end
+
+    input1_array = @input1.downcase.chars.sort
+    input2_array = @input1.downcase.chars.sort
     if input1_array.all? { |i| input2_array.include?(i)}
       puts "These words have no matching letters and are considered to be ANTIGRAMS! The spiritual opposite of an anagram..."
       return "These words have no matching letters and are considered to be ANTIGRAMS! The spiritual opposite of an anagram..."
     end
+
   end
 end
+# ("Dormitory!", "Dirty room??")
+  # x = "There$ a t%n of Sh!t here@"
+  # x.gsub(/\W/, ' ').delete(' ')
+  # s = "I have white space".delete(' ')
 
-
-input1.each do
-      if string[counter]=="a" || string[counter]=="e" || string[counter]=="i" || string[counter]=="o" || string[counter]=="u"
-return "Type in a real words, you bozo."
-puts "Type in a real words, you bozo."
-
-
-# vowel_array1 = input1.scan(/[aeiou]/)
-# vowel_array2 = input2.scan(/[aeiou]/)
-#
-# if vowel_array1 || vowel_array2 == []
-#   puts "Type in a real words, you bozo."
-#   return "Type in a real words, you bozo."
+  #
+  #
+  # def vowel_checker()
+  #   if @input1.scan(/[aeoui]/).count == 0
+  #     return "Type in a real words, you bozo."
+  #     puts "Type in a real words, you bozo."
+  #   end
+  #   if @input2.scan(/[aeoui]/).count == 0
+  #     return "Type in a real words, you bozo."
+  #     puts "Type in a real words, you bozo."
+  #   end
+  #   if @input1.scan(/[aeoui]/).count != 0
+  #     return "this is a words"
+  #     puts "this is a word"
+  #     # self.check_if_anagram(@input1, @input2)
+  #   end
+  #   if @input2.scan(/[aeoui]/).count != 0
+  #     return "this is a words"
+  #     puts "this is a word"
+  #     # self.check_if_anagram(@input1, @input2)
+  #   end
+  # end
+  #
--- spec/anagramer_spec.rb
+++ spec/anagramer_spec.rb
@@ -5,19 +5,23 @@ require ('anagramer')
 
 describe("Anagramer#check_if_anagram") do
   # it ("takes two words and determines if they are actual words via vowel check") do
-  #   anagram1 = Anagramer.new()
-  #   expect(anagram1.check_if_anagram("btb","tpk")).to(eq("Type in a real words, you bozo."))
+  #   anagram1 = Anagramer.new("kkk","kid")
+  #   expect(anagram1.check_if_anagram()).to(eq("Type in a real words, you bozo."))
   # end
-  it ("determines if two words are not anagrams by length") do
-    anagram1 = Anagramer.new()
-    expect(anagram1.check_if_anagram("rasst","tat")).to(eq("This is NOT an anagram!"))
-  end
+  # it ("determines if two words are not anagrams by length") do
+  #   anagram1 = Anagramer.new("rasst","tat")
+  #   expect(anagram1.check_if_anagram()).to(eq("This is NOT an anagram!"))
+  # end
   it ("determines if two words are anagrams.") do
-    anagram1 = Anagramer.new()
-    expect(anagram1.check_if_anagram("rat","tar")).to(eq("This is an ANAGRAM!"))
+    anagram1 = Anagramer.new("rat","tar")
+    expect(anagram1.check_if_anagram()).to(eq("This is an ANAGRAM!"))
+  end
+  it ("determines if multiple worded 2 sentence inputs are anagrams") do
+    anagram1 = Anagramer.new("Dormitory!", "Dirty room??")
+    expect(anagram1.check_if_anagram()).to(eq("This is an ANAGRAM!"))
   end
   it ("determines if two words are antigrams") do
-    anagram1 = Anagramer.new()
-    expect(anagram1.check_if_anagram("Tank","Quiz")).to(eq("These words have no matching letters and are considered to be ANTIGRAMS! The spiritual opposite of an anagram..."))
+    anagram1 = Anagramer.new("Tank","Quiz")
+    expect(anagram1.check_if_anagram()).to(eq("These words have no matching letters and are considered to be ANTIGRAMS! The spiritual opposite of an anagram..."))
   end
 end
