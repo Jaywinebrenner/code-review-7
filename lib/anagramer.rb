e16dc417ab7051451823324dae403f0ab758219f
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-12-13 09:44:29 -0800

    pass test for determining if 2 words are anagrams

--- lib/anagramer.rb
+++ lib/anagramer.rb
@@ -23,14 +23,16 @@
 
 
 class Anagramer
-  
+
   def check_if_anagram (input1, input2)
     if input1.length != input2.length
       puts "This is NOT an anagram"
+      return "This is NOT an anagram!"
     end
     if input2.downcase.chars.sort == input1.downcase.chars.sort
       puts "This is an ANAGRAM!"
+      return "This is an ANAGRAM!"
     end
-    return
+
   end
 end
